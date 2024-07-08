b82dcccc169e84d2d0e85019cdac171eb808e950
unknown <dleitedias@gmail.com>
2024-07-07 22:22:10 -0300

    Jogo número secreto: versão 4

--- Atividades de fixação/1-jogo-numero-secreto/app.js
+++ Atividades de fixação/1-jogo-numero-secreto/app.js
@@ -1,14 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1; 
 
-while(chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 30');
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
 
-    if(numeroSecreto == chute) {
-        alert('Isso aí, você descobriu o numero secreto ' + numeroSecreto + ' na tentativa ' + tentativas );
+    if(chute == numeroSecreto ) {
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert('O número secreto é menor que ' + chute);
@@ -18,4 +18,10 @@ while(chute != numeroSecreto) {
         tentativas ++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert('Isso aí, você descobriu o numero secreto ' + numeroSecreto + ' com ' + tentativas + ' ' + palavraTentativa);
+
+
+
 
