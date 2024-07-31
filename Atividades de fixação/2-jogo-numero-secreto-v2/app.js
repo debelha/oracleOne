8b7e7f4bda7be27e45799840b74be653304107e3
unknown <dleitedias@gmail.com>
2024-07-30 22:54:09 -0300

    Jogo do número secreto 2

---
+++ Atividades de fixação/2-jogo-numero-secreto-v2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
