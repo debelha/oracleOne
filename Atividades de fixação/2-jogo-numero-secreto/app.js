ab53f5062c210996557d57d24d459754f6198bf7
unknown <dleitedias@gmail.com>
2024-07-31 17:02:03 -0300

    Jogo do número secreto 2.1

---
+++ Atividades de fixação/2-jogo-numero-secreto/app.js
@@ -0,0 +1,18 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1); 
+}
