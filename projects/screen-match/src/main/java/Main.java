7b841df1304953583aad97534009a3387a43623d
unknown <dleitedias@gmail.com>
2024-10-07 21:27:47 -0300

    add variables

---
+++ projects/screen-match/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+     int anoLancamento = 2022;
+     boolean incluindoNoPlano = true;
+     double notaDoFilme = 8.1;
+
+     System.out.println("Ano lançamento: " + anoLancamento);
+    }
+}
+
