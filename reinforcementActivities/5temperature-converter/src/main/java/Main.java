6b149640a8cf4ad352ab122fb4e4966aaac679bd
unknown <dleitedias@gmail.com>
2024-10-15 21:45:31 -0300

    add task3: temperature converter

---
+++ reinforcementActivities/5temperature-converter/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double celsius = 12.8;
+        int fahrenheit = (int) (celsius * 1.8 + 32);
+        System.out.println(fahrenheit);
+    }
+}
