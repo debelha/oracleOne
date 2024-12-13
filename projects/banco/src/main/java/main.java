58e0d3f25c1c9be28e4d4a764b1bf88741ef1213
unknown <dleitedias@gmail.com>
2024-12-12 22:12:47 -0300

    inicializando dados

---
+++ projects/banco/src/main/java/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        String nome = "Deborah";
+        String tipoConta = "Corrente";
+        double saldo = 1500.00;
+
+        System.out.println("***********************");
+        System.out.println("\nNome: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("\n***********************");
+    }
+}
