c19fcc668accbd9eeaae7ba552ae0fd311a51d47
unknown <dleitedias@gmail.com>
2025-01-06 20:56:56 -0300

    add menu

--- projects/banco/src/main/java/main.java
+++ projects/banco/src/main/java/main.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         String nome = "Deborah";
         String tipoConta = "Corrente";
         double saldo = 1500.00;
+        int opcao = 0;
 
         System.out.println("***********************");
         System.out.println("\nNome: " + nome);
         System.out.println("Tipo de conta: " + tipoConta);
         System.out.println("Saldo: " + saldo);
         System.out.println("\n***********************");
+
+        String menu = """
+                ** Digite sua opção: **
+                1- Consultar saldo
+                2- Transferir valor
+                3- Receber valor
+                4- Sair
+               
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+        }
     }
 }
