cef39d9266490412d44e330c7ccac371ece99e2d
unknown <dleitedias@gmail.com>
2025-01-14 22:46:22 -0300

    add class main

---
+++ projects/screenmatch/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Filme filme1 = new Filme();
+        filme1.titulo = "Glass Onion";
+        filme1.ano = 2020;
+        filme1.duracao = 190;
+
+        System.out.println(filme1.titulo);
+        System.out.println(filme1.ano);
+    }
+}
