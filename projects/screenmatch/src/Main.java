cf01925f9180436f75da649c0d06f2f15f8ea016
unknown <dleitedias@gmail.com>
2025-01-19 17:32:34 -0300

    add class serie

--- projects/screenmatch/src/Main.java
+++ projects/screenmatch/src/Main.java
@@ -1,11 +1,13 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
         filme1.setNome("Glass Onion");
         filme1.setAno(2020);
-        filme1.setDuracao(190);
+        filme1.setDuracaoEmMinutos(190);
+        System.out.println("Duração do filme: " + filme1.getDuracaoEmMinutos());
 
         filme1.exibeFichaTecnica();
         filme1.avalia(8);
@@ -14,5 +16,14 @@ public class Main {
         System.out.println("Total de avaliações: " + filme1.getTotalAvaliacoes());
         System.out.println(filme1.pegaMedia());
         System.out.println(filme1.pegaMedia());
+
+        Serie serie1 = new Serie();
+        serie1.setNome("Vis a vis");
+        serie1.setAno(2020);
+        serie1.exibeFichaTecnica();
+        serie1.setTemporadas(5);
+        serie1.setEpisodiosPorTemporada(10);
+        serie1.setMinutosPorEpisodio(50);
+        System.out.println("Duração da série: " + serie1.getDuracaoEmMinutos());
     }
 }
---
+++ projects/screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,60 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+    private String nome;
+    private int ano;
+    private boolean inclusoNoPlano;
+    private double somaAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public boolean getInclusoNoPlano() {
+        return inclusoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalAvaliacoes(){
+        return totalAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setInclusoNoPlano(boolean inclusoNoPlano) {
+        this.inclusoNoPlano = inclusoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + ano);
+    }
+
+    public void avalia(double nota){
+        somaAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaAvaliacoes / totalAvaliacoes;
+    }
+}
